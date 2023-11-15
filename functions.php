b6fa247965c4f5f2f793d316beddb15efff19cf5
iliebostan94 <iliebostan@gmail.com>
2023-11-15 10:36:02 +0200

    functions.php updated [facing_dev_1]

--- functions.php
+++ functions.php
@@ -63,3 +63,25 @@ collect(['setup', 'filters'])
             );
         }
     });
+
+//support for custom logo WP
+add_theme_support( 'custom-logo' );
+
+//function to add custom classes to main nav menu <li> items
+function add_additional_class_on_li($classes, $item, $args) {
+    if(isset($args->add_li_class)) {
+        $classes[] = $args->add_li_class;
+    }
+    return $classes;
+}
+add_filter('nav_menu_css_class', 'add_additional_class_on_li', 1, 3);
+
+function add_custom_new_menu() {
+    register_nav_menus(
+        array(
+            'footer-menu-1' => __( 'Footer Menu 1' ),
+            'footer-menu-2' => __( 'Footer Menu 2' )
+        )
+    );
+}
+add_action( 'init', 'add_custom_new_menu' );
